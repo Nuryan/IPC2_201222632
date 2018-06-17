bf2042dedbf67aefde3cef976b913f0ff7c3aa5f
Abner Misael Paredes de la Cruz <201222632@ingenieria.usac.edu.gt>
2018-06-16 23:12:48 -0600

    Tarea 2

---
+++ Fase 2/DB/Scripts/script1.sql
@@ -0,0 +1,37 @@
+create table cargo(
+	idCargo serial,
+	nombre varchar(25),
+	constraint cargo_pk primary key(idCargo)
+);
+
+insert into cargo(nombreCargo) values ('Usuario Ninty'), ('Administrador')
+
+select * from cargo
+
+create table Persona(
+	idPersona serial,
+	dpi bigint unique,
+	nombres varchar(25),
+	apellidos varchar(25),
+	organizacion varchar(25),
+	contraseña text,
+	nacimiento date,
+	residencia varchar(75),
+	idCargo integer,
+	constraint persona_pk primary key (idPersona),
+	constraint persona_fk_cargo foreign key (idCargo) references cargo(idCargo)	
+);
+
+insert into Persona(dpi, nombres, apellidos, organizacion, contraseña, nacimiento, residencia, idCargo)
+values (1234567890123, 'Admin1 NOmbre','Admin1 Apellido', 'Ninty', md5('asdf'), '1995/12/31', 'Su casa!', 2 )
+
+select *, cargo.nombre from persona inner join cargo on persona.idCargo = cargo.idCargo
+
+
+
+
+
+
+
+
+
